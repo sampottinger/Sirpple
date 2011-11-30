8e98ac883898c9bc4dbe55b90853cb58e9122caa
DJ Sutton <dj@laptop>
2011-11-29 22:43:55 -0700

    added limejs library as a git submodule
    To setup limejs run utils/update_lime.sh

---
+++ utils/update_lime.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# get script directory
+SOURCE="$0"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# cd to proj root
+cd $DIR/..
+if [ ! -f Sirpple/jslibs/limejs/.git ]; then
+    git submodule update --init
+else
+    cd Sirpple/jslibs/limejs
+    git reset --hard HEAD
+    cd ../../..
+    git submodule update --rebase
+fi
+
+#cd to limejs
+cd Sirpple/jslibs/limejs
+git checkout master
+
+if [ ! -f closure ]; then
+    # closure does not exist , so init limejs
+    bin/lime.py init
+else
+    cd closure
+    #check for updates to closure
+    git reset --hard HEAD
+    git svn rebase
+    ../bin/lime.py update
+fi
