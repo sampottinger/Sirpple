d2b63b3315778fb3a9f72c590cc4d5247de4df16
A. Samuel Pottinger <Samnsparky@gmail.com>
2011-12-05 21:56:27 -0700

    Rewrote ast builder to use get_children

--- Sirpple/compiler/ast.py
+++ Sirpple/compiler/ast.py
@@ -1,48 +1,105 @@
-#!/usr/bin/env python
-
-import runtime
-sys.path.append('..')
-from configparser import get_parser
+"""
+Set of classes to build an Abstract Syntax Tree for the game compiler 
+"""
+from serialization import model_graph
+from serialization import backends
 
 class Node(object):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
-def get_collection_name(class_name):
-    name = ''.join([c if c.islower() else '_'+c.lower() for c in s])
-    return name.lstrip('_')+'s'
+# Singleton might make sense here since graph is actually a bit intensive to load
+class NodeFactory:
+    """ Factory to build the nodes of the AST """
 
-def build(project):
-    
-    parser = get_parser('yaml')
-    app_config = parser.load_file('app_config.yaml')
-    classes = parser.load_dir(app_config['classes'])
-    
-    return build_node(project)
+    __instance = None
 
-def build_node(model, classes):
-    name = model.__class__.__name__
-    fields={}
+    @classmethod
+    def get_instance(self):
+        """
+        Get a shared instance of this NodeFactory singleton
+
+        @return: Shared NodeFactory instance
+        @rtype: NodeFactory
+        """
+        if NodeFactory.__instance == None:
+            NodeFactory.__instance = ConfigModelFactory()
+        
+        return NodeFactory.__instance
     
-    for class_name, class_body in classes.items():
-        if class_body['parent'] == name # Can give DJ this 
-        
-        field = get_collection_name(class_name)
-        model_class = globals()[class_name] # Give DJ real class
-        
-        collection = []
-        for child_model in model.get_children(model_class):
-            collection.append(build_node(child_model))
-        
-        fields[field] = collection
+    def __init__(self):
+
+        self.__graph = model_graph.ModelGraph.get_current_graph()
+
+        db_manager = backends.DatabaseManager.get_instance()
+        self.__collection_types = db_manager.get_collection_property_names()
     
-    # use Sam's stuff for this
-    for field, value in model.__dict__.items():
-        
-        if value is ReferenceProperty:
-            fields[field] = build_node(something)
-        else:
-            fields[field] = value
-    # end use Sam's stuff for this
+    def get_collection_name(self, name):
+        name = ''.join([c if c.islower() else '_'+c.lower() for c in name])
+        return name.lstrip('_')+'s'
     
-    retrun Node(**fields)
+    # TODO: Remove this if no longer necessary
+    def create_class_tree(self, target_model):
+        """
+        Create a new AST containing class information from the given class
+
+        @param target_model_class: The model class at the root of the tree
+        @type target_model: Any model class object
+        @return: Root of AST that corresponds to the given target_model
+        @rtype: Node
+        """
+        raise NotImplementedError("Stub for future functionality")
+    
+    def create_tree(self, target_model):
+        """
+        Create a new AST rooted at the specified model instance
+
+        @param target_model: The instance to build the AST form
+        @type target_model: Any model instance
+        @return: Root of AST that corresponds to the given target_model
+        @rtype: Node
+        """
+
+        # Get class definition
+        class_name = target_model.__class__.__name__
+        class_definition = self.__graph.get_class_definition(class_name)
+
+        node_fields = {}
+
+        for field_defn in class_definition.get_fields().vals():
+            node_fields[field_defn.get_name()] = self.__create_tree_representation(target_model, field_defn)
+    
+    def __create_tree_representation(self, target_model, field_definition):
+        """
+        Determines the appropriate representation for the given field
+
+        @param target_model: The model from which the target field came
+        @type target_model: Any model instance
+        @param field_definition: The definition of the field to create a 
+                                 representation for in the target tree
+        @type field_definition: FieldDefinition
+        @return: The appropriate representation and data for given model instance
+        @rtype: Node, Collection, or primitive
+        """
+
+        field_type = field_defn.get_field_type()
+
+        # Make node for pointer or collection
+        if field_type.is_reference():
+
+            field_name = field_defn.get_name()
+
+            # Check to see if it should become a collection
+            if field_name in self.__collection_types:
+                collection = []
+                for child_model in graph.get_children(target_model):
+                    collection.append(self.create_node(child_model))
+                return collection
+            
+            # Otherwise save as pointer
+            else:
+                return self.create_tree(getattr(target_model, field_name))
+        
+        # Get value for primitive
+        else:   
+            return getattr(target_model, field_name)
