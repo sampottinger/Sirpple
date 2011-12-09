0750f59c7e93468f2d7f99511e69e54c2771ac3a
DJ Sutton <dj@laptop>
2011-12-09 13:45:56 -0700

    nifty little testing framework
    to use, start GAE and visit
    http://localhost:808/test?module=<module name>&test=<test name>
    
    tests can be added as py files in the test directory and then
    imported in __init__.py

---
+++ Sirpple/test/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+sys.path.append(os.getcwd())
+
+import dj
---
+++ Sirpple/test/dj.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import sys
+
+from serialization.config_model import ConfigModelFactory
+from serialization import model_graph
+from google.appengine.ext.db import Query
+#from compiler import astbuilder
+
+import yamlmodels
+
+globals().update(yamlmodels.load())
+
+def populate_db():
+    proj = Project()
+    proj.name = 'Test Project'
+    proj.put()
+    
+    world = World(parent=proj)
+    world.name = 'World 1'
+    world.put()
+    
+    proj.starting_world = world
+    proj.put()
+    
+    cons = WorldMethod(parent=world)
+    cons.name = 'World 1 constructor'
+    cons.signature = '{}'
+    cons.body_type = 'raw'
+    cons.body = 'document.write("hello Sirpple")'
+    cons.put()
+    
+    world.constructor = cons
+    world.put()
+
+def setup_db():
+    factory = ConfigModelFactory.get_instance()
+    project_model = factory.get_model('Project')
+    
+    projects = project_model.all()
+    if not projects.get():
+        populate_db()
+
+def children(handler):
+
+    setup_db()
+
+    factory = ConfigModelFactory.get_instance()
+    project_model = factory.get_model('Project')
+    
+    projects = project_model.all()
+    project = projects.get()
+    
+    children = model_graph.ModelGraph.get_current_graph().get_children(project)
+    print >>handler, 'Children test passed'
+
