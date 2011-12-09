8979fea37e059db2330bd3881de1f4e93629d030
A. Samuel Pottinger <Samnsparky@gmail.com>
2011-12-09 14:02:33 -0700

    Start on type converters for data transfer objects

--- Sirpple/serialization/dto.py
+++ Sirpple/serialization/dto.py
@@ -1,11 +1,13 @@
-""" Data transfer strctures for serialization """
+""" Mechanisms for supporting data transfer objects """
 
+import logging
 from serialization import model_graph
 
 class DTOBuilder:
-    """ Builder that produces data transfer objects """
+    """ Builder that produces and reads data transfer objects """
 
     CLASS_IDENTIFIER = "__class__"
+    CHILDREN_PREFIX = "children_"
 
     __instance = None
 
@@ -43,9 +45,56 @@ class DTOBuilder:
         ret_dict[DTOBuilder.CLASS_IDENTIFIER] = class_name
 
         # Handle basic attributes
-        for field in class_definition.get_fields():
+        for field in filter(lambda x: x.is_exposed(), class_definition.get_fields()):
             field_name = field.get_name()
             ret_dict[field_name] = getattr(target, field_name)
         
         # Handle children
-        graph.get_children()
+        children = graph.get_children(target)
+        
+        # Fill in the children instances
+        for defn, instance in children.items():
+            id_list = map()
+            ret_dict[CHILDREN_PREFIX + defn.get_name().lower()] = id_list
+        
+        return ret_dict
+    
+    def read_dto(self, target, class_definition, target=None):
+        """
+        Reads out from a DTO to generate a fully instantiated instance
+
+        @param source: The dictionary to parse out
+        @type source: Dictionary
+        @param class_definition: The definition of the class to read from
+        @type class_definition: ClassDefinition
+        @keyword target: The instance to read into (update). If None, will
+                         create a new instance. Defaults to None.
+        @type target: Boolean
+        @return: Fully instantiated (but not saved)
+        @rtype: Instance of the class class_defn represents or None on fail
+        """
+
+        # Get actual class
+        target_class = class_defn.get_class()
+
+        # Get field definitions from class defintion
+        field_definitions = class_definition.get_fields()
+
+        # Create structue to hold cleaned fields
+        cleaned_foreign_entries = {}
+
+        # Clean and check for editing non-exposed fields
+        for foreign_field, foreign_value in target:
+
+            # Check that the field is available
+            if not foriegn_field in field_definitions:
+                logging.error("Invalid field " + foreign_field + " passed to " + target_class.get_name())
+                return None
+            
+            # Check that the field is exposed
+            field_defintion = field_defintions[foreign_field]
+            if not field_definition.is_exposed():
+                logging.error("Attempted to write non-exposed field " + field_defintion.get_name() + " for " + target_class.get_name() + " through REST API")
+            
+            # Clean and store
+            cleaned_foriegn_entries[foreign_field] = cgi.escape()
