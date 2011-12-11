933b433bf91afc45a33abd196b3ce93e23a6f9ab
DJ Sutton <dj@laptop>
2011-12-11 01:18:12 -0700

    added toplevel Compiler class

--- Sirpple/compiler/__init__.py
+++ Sirpple/compiler/__init__.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python
+
+from ast import NodeFactory
+from visitors.rename import RenameVisitor
+from visitors.render import RenderVisitor
+
+class Compiler(object):
+    
+    def compile(self, project):
+        
+        node_factory = NodeFactory.get_instance()
+        
+        project = node_factory.create_tree(project)
+        
+        project = RenameVisitor().visit(project)
+        
+        project_txt = RenderVisitor().visit(project)
+        node_factory.clear_node_cache()
+        return project_txt
+        
